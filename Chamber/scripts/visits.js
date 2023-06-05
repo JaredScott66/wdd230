701b52a78d2f99ab770b259225a339563f772876
JaredScott66 <jaredstscott@juno.com>
2023-06-04 23:40:04 -0500

    ss

---
+++ Chamber/scripts/visits.js
@@ -0,0 +1,9 @@
+const visitsDisplay = document.querySelector(".visits")
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+visitsDisplay.textContent = numVisits;
+numVisits ++;
+
+localStorage.setItem("visits-ls", numVisits);
+
