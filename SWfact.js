0de7229c549a112878dde12368e69194595a7185
JaredScott66 <jaredstscott@juno.com>
2023-06-16 00:44:51 -0500

    gone

--- SWfact.js
+++ SWfact.js
@@ -1,17 +1,13 @@
 const url = "https://swapi.dev/api/people/1/";
 var outputElement = document.querySelector("#data");
 
-function addSite() {
-    // outputElement.innerHTML = `${facts}`;
-    console.log = data;
-    
-}
+
 
 async function factGrap(url) {
     const response = await fetch(url);
     const facts = await response.json();
     outputElement.innerHTML = facts;
-    // addSite();
+
     console.log = facts;
 }
 
