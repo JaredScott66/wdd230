632a16c006b5e7674942dff89e08dff6fbedd409
JaredScott66 <jaredstscott@juno.com>
2023-07-13 21:34:21 -0500

    ehh

--- Bountiful Foods/scripts/getFruitData.js
+++ Bountiful Foods/scripts/getFruitData.js
@@ -13,17 +13,28 @@ const displayFruitSelection = (fruits) => {
 
     idList.forEach((element) => {
         const fruitList = document.getElementById(element);
-
+        
         fruits.forEach((fruit) => {
             let item = document.createElement("option");
 
             item.setAttribute("value", fruit.name);
-            item.innerText = `Select`;
+            item.innerText = `${fruit.name}`;
 
             fruitList.append(item);
         });
 
     });
+}
+
+const getNutrition = (fruits) => {
+    const btn = document.getElementsByName(fruitSubmit);
+    const fruit1 = document.getElementById("fruitSelect1");
+    const fruit2 = document.getElementById("fruitSelect2");
+    const fruit3 = document.getElementById("fruitSelect3");
+
+    let div = document.createElement("div");
+    let 
+
 }
 
 getData();
